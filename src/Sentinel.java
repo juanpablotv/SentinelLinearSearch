714f7ad8aa773aab7126b8d4adae83d8729ff762
juanpablotv <torresvaldesjuanpablo@gmail.com>
2023-11-15 23:02:02 -0600

    Metodo CLS verificado

--- src/Sentinel.java
+++ src/Sentinel.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
+
 
 public class Sentinel {
-
     public static int search(int[] array, int target) {
         int lastIndex = array.length - 1;
 
@@ -28,44 +27,35 @@ public class Sentinel {
             return -1; // El valor objetivo no está presente en el array
         }
     }
+    
+    
 
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader bufer = new BufferedReader(
-            new InputStreamReader(System.in));
-            String  pregunta; 
+        System.out.print("Ingrese el tamaño del arreglo: ");
+        int size = Integer.parseInt(reader.readLine());
 
-            System.out.println("Presione Y si quiere leer un archivo en memoria");
-            pregunta = bufer.readLine();
+        int[] array = new int[size];
 
-            if (pregunta == "Y") {
-
-            } else {
-            
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-             System.out.print("Ingrese el tamaño del array: ");
-              int size = Integer.parseInt(reader.readLine());
-
-              int[] array = new int[size];
-
-             System.out.println("Ingrese los elementos del array :");
-             for (int i = 0; i < size; i++) {
-                 System.out.print("Elemento " + (i + 1) + ": ");
-                 array[i] = Integer.parseInt(reader.readLine());
-            }
-
-        
-                System.out.print("Ingrese el numero a buscar: ");
-                int target = Integer.parseInt(reader.readLine());
-
-                int result = search(array, target);
-
-                if (result != -1) {
-                     System.out.println("Elemento encontrado en el arreglo: " + result);
-                } else {
-                     System.out.println("Elemento no encontrado en el arreglo.");
-                }
+        System.out.println("Ingrese los elementos del arreglo (uno por uno):");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            array[i] = Integer.parseInt(reader.readLine());
         }
-    }   
+
+        System.out.print("Ingrese el valor a buscar: ");
+        int target = Integer.parseInt(reader.readLine());
+
+        int result = search(array, target);
+
+        if (result != -1) {
+            System.out.println("Elemento encontrado en el índice: " + result);
+        } else {
+            System.out.println("Elemento no encontrado en el arreglo.");
+        }
+    }
 }
+
+
+    
